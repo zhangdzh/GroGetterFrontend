8b22fd69fd379e24517940b9ae86d2da21045d0f
junhkim20 <jk7048@nyu.edu>
2023-02-19 11:59:48 -0500

    Made App.js work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from 'logo.svg';
+// import 'App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <ul>
-          <li class="dropdown">
-              <a href="index.html">Home</a>
-          </li>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <ul>
+//           <li class="dropdown">
+//               <a href="index.html">Home</a>
+//           </li>
 
-          <li class="dropdown">
-              <a href="#">Lists</a>
-              <div class="dropdown-content">
-                  <a href="manageList.html">Manage</a>
-                  <a href="addList.html">Add</a>
-              </div>
-          </li>
-        </ul>
-      </header>
-    </div>
-  );
-}
+//           <li class="dropdown">
+//               <a href="#">Lists</a>
+//               <div class="dropdown-content">
+//                   <a href="manageList.html">Manage</a>
+//                   <a href="addList.html">Add</a>
+//               </div>
+//           </li>
+//         </ul>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('GroGetter');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
