f665658d7650e2033b2b05c4429ffd2af2181b97
junhkim20 <jk7048@nyu.edu>
2023-02-19 15:24:55 -0500

    NavBar + Animation

--- public/App.js
+++ public/App.js
@@ -1,41 +1,23 @@
-// import logo from 'logo.svg';
-// import 'App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <ul>
-//           <li class="dropdown">
-//               <a href="index.html">Home</a>
-//           </li>
-
-//           <li class="dropdown">
-//               <a href="#">Lists</a>
-//               <div class="dropdown-content">
-//                   <a href="manageList.html">Manage</a>
-//                   <a href="addList.html">Add</a>
-//               </div>
-//           </li>
-//         </ul>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('GroGetter');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const navSlide = () => {
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".nav-links");
+	const navLinks = document.querySelectorAll(".nav-links a");
+  
+	burger.addEventListener("click", () => {
+	  nav.classList.toggle("nav-active");
+  
+	  navLinks.forEach((link, index) => {
+		if (link.style.animation) {
+		  link.style.animation = "";
+		} else {
+		  link.style.animation = `navLinkFade 0.5s ease forwards ${
+			index / 7 + 0.5
+		  }s `;
+		}
+	  });
+	  burger.classList.toggle("toggle");
+	});
+	//
+};
+  
+navSlide();
