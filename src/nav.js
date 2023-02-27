f622de34582cb8eb945362565744170c4afacf8f
tse-jeff <jeff.tse@nyu.edu>
2023-02-26 23:52:30 -0500

    started a navbar react component

---
+++ src/nav.js
@@ -0,0 +1,23 @@
+// react component for the navigation bar
+import React from 'react';
+// import { Link } from 'react-router-dom';s
+
+const Nav = () => {
+  return (
+    <nav>
+      <div class="logo">
+        <h4>GroGetter</h4>
+      </div>
+      {/* change this to use the react router */}
+      <ul class="nav-links">
+        <a href="index.html">Home</a>
+        <a href="about.html">About Us</a>
+        <a href="addList.html">Add List</a>
+        <a href="manageList.html">Manage List</a>
+
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav;
