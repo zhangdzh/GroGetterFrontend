5b2596d3cc1f088e4de580338cdc5f5a69b6cbcf
tse-jeff <jeff.tse@nyu.edu>
2023-03-12 21:50:26 -0400

    new grocery list component

---
+++ src/groclist.js
@@ -0,0 +1,15 @@
+// possibly the component to display the grocery lists?
+
+import React from 'react';
+import './style.css';
+
+
+const GrocList = () => {
+  return (
+    <div class="groceryList">
+      <h1>Grocery List</h1>
+    </div>
+  )
+}
+
+export default GrocList;
