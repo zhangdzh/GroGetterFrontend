eaa8d20a576e2b3bf3b81853beb3a6dcad4a573b
tse-jeff <jeff.tse@nyu.edu>
2023-03-26 23:32:35 -0400

    form for sign up and  states to store info to POST

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // not sure what our MongoDB database link is
 const backendURL = 'http://localhost:3000';
 
 const SignUp = () => {
+	const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [message, setMessage] = useState('');
 
 	// call the users add endpoint using fetch
-	const addUser = async (username, email, password) => {
-		// might need to make the strings into fields
-		await fetch(`${backendURL}/users/add`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			// this is the body of the POST request
-			body: JSON.stringify({
-				username: username,
-				email: email,
-				password: password
-			})
-		});
+	const addUser = async (e) => {
+		e.preventDefault();
+		try {
+			// TODO: make text into field variables
+			let res = await fetch(`${backendURL}/users/add`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				// body of the POST request
+				body: JSON.stringify({
+					username: username,
+					email: email,
+					password: password
+				})
+			});
+			// not sure of purpose of this
+			let resJson = await res.json();
+
+			// reset states if ok
+			if (res.status === 200) {
+				setName("");
+				setEmail("");
+				setPassword("");
+				setMessage("User created successfully");
+			} else {
+				setMessage("Some error occured");
+			}
+		} catch (err) {
+			console.log(err);
+		}
 	};
 
 	return (
@@ -32,6 +53,31 @@ const SignUp = () => {
 			}}
 		>
 			<h1>New User: Sign Up</h1>
+			{/* form that calls addUser once submitted */}
+			<form onSubmit={addUser}>
+				<input
+					type='text'
+					placeholder='Username'
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<input
+					type='text'
+					placeholder='Email'
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<input
+					type='text'
+					placeholder='Password'
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<button type='submit'>Sign Up</button>
+
+				{/* creates an alert if there is an error */}
+				<alert>{message ? <p>{message}</p> : null}</alert>
+			</form>
 		</div>
 	);
 };
