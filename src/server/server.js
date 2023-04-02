089175d452831596ec572446ccbcb9b929dbaa51
Yeseon02 <yk2384@nyu.edu>
2023-04-02 16:54:43 -0400

    adding environment variable for api platform

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,18 @@
 const port = process.env.PORT || 3000;
+const express = require('express');
+const grogetter = require('grogetter-sdk');
 
-//connecting to api platform using heroku
+//TODO:connect to api platform using heroku
+app.get("/", (req, res) => {
+    res.send("Hello World"); //change
+});
+
 app.listen(port, () => {
-  console.warn(`App listening on http://localhost:${PORT}`);
+  console.warn(`App listening on http://localhost:${port}`);
+});
+
+//creating env variables
+let SWE22 = new grogetter.SWE22({
+  accessKeyId: process.env.SWE22_KEY,
+  secretAccessKey: process.env.SWE22_SECRET
 });
