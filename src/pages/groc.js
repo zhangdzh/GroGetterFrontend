7425f10bb173325fa3ae4f7c1b92a98b5e4d066b
tse-jeff <jeff.tse@nyu.edu>
2023-04-02 21:20:27 -0400

    start of groc/items api call

--- src/pages/groc.js
+++ src/pages/groc.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// python anywhere backend link
+const backendURL = 'zhangdzh.pythonanywhere.com';
 
 const Groc = () => {
+
+	const [groceryList, setGroceryList] = useState([]);
+
+	const getGroc = async (e) => {
+		e.preventDefault();
+		try {
+			let res = await fetch(`${backendURL}/groc/items`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			let resJson = await res.json();
+			setGroceryList(resJson);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+
 return (
 	<div
 	style={{
