0fb26a6aabb5b8ba58a2d3a5b9f5af0b6ab699ff
Yeseon02 <yk2384@nyu.edu>
2023-04-08 16:21:55 -0400

    added signin dropdown

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,4 +1,5 @@
 // import React, { useState } from 'react';
+import { Dropdown, Option } from "../components/Dropdown/dropdown";
 
 const SignIn = () => {
 
@@ -30,11 +31,19 @@ return (
 	style={{
 		display: 'flex',
 		justifyContent: 'Left',
+		flexDirection: 'Column',
 		alignItems: 'Left',
-		height: '10vh'
+		height: '10vh',
+		paddingLeft: '10rem'
 	}}
 	>
 	<h1>Returning User: Sign In</h1>
+	<Dropdown>
+        <Option selected value="Usernames" />
+        <Option value="Option 1" />
+        <Option value="Option 2" />
+        <Option value="Option 3" />
+      </Dropdown>
 	</div>
 );
 };
