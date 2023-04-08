0c0ca4a98c1422e1d926a4c5960cfabb265b4db6
junhkim20 <jk7048@nyu.edu>
2023-04-08 16:08:14 -0400

    Fetch from PythonAnywhere for SignIn

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,6 +1,30 @@
-import React from 'react';
+// import React, { useState } from 'react';
 
 const SignIn = () => {
+
+// python anywhere backend link
+const backendURL = 'zhangdzh.pythonanywhere.com';
+
+const getUserList = async () => {
+	const settings = {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }
+    };
+    try {
+        const fetchResponse = await fetch(`https://${backendURL}/users/dict/`, settings);
+        const data = await fetchResponse.json();
+		console.log(data); 
+        return data;
+    } catch (e) {
+        return e;
+    }    
+};
+
+getUserList(); 
+
 return (
 	<div
 	style={{
