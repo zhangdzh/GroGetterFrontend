cd29b1a3e74ddbecefbdd244d17b54f7e2557151
tse-jeff <jeff.tse@nyu.edu>
2023-04-09 21:22:17 -0400

    added a button for testing grocDict endpoint

--- src/pages/groc.js
+++ src/pages/groc.js
@@ -23,19 +23,49 @@ const Groc = () => {
 		}
 	};
 
+	// test function for the grocDict endpoint
+	// which uses the get_grocery_list function
+	const [groceryDict, setGrocDict] = useState({});
+	const grocDict = async (e) => {
+		e.preventDefault();
+		try {
+			let res = await fetch(`${backendURL}/groc/dict`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			let resJson = await res.json();
+			setGrocDict(resJson);
+		} catch (err) {
+			// console.log(err);
+		}
+	};
 
-return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Left',
-		alignItems: 'Left',
-		height: '10vh'
-	}}
-	>
-	<h1>Manage your Grocery List here!</h1>
-	</div>
-);
+
+	return (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'Left',
+				alignItems: 'Left',
+				height: '10vh'
+			}}
+		>
+			<h1>Manage your Grocery List here!</h1>
+
+			{/* button that calls grocDict function */}
+			<button onClick={grocDict}>Get Grocery Dictionary</button>
+			<ul>
+				{Object.entries(groceryDict).map(([key, value]) => (
+					<li key={key}>
+						<strong>{key}: </strong>
+						{JSON.stringify(value)}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default Groc;
