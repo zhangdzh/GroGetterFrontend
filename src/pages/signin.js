b36a1450cdb3c2a300a2cdec4aef609848c24874
junhkim20 <jk7048@nyu.edu>
2023-04-26 09:20:32 -0400

    Sign In endpoint

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -6,30 +6,73 @@ const SignIn = () => {
 
     // python anywhere backend link
     const backendURL = 'zhangdzh.pythonanywhere.com';
-    const [users, setUsers] = useState([]);
+    // const [users, setUsers] = useState([]);
 
-    useEffect( () => {
-        async function fetchData() {
-            try {
-                const batchTrack = document.getElementById("batchSelect");
-                const fetchResponse = await fetch(`https://${backendURL}/users/list`);
-                const users = await fetchResponse.json();
-                // const users = Object.keys(data['Data'])
-                setUsers(users); 
+    const [input, setInput] = useState({
+		username: '',
+		password: ''
+	});
 
-                for (let i = 0; i < users.length; i++) {
-                    const newOption = document.createElement("option");
-                    newOption.value = users[i];
-                    newOption.text = users[i];
-                    batchTrack.appendChild(newOption);
-                }
+    const onInputChange = e => {
+		const { name, value } = e.target;
+		setInput(prev => ({
+			...prev,
+			[name]: value
+		}));
+	}
 
-            } catch (err) {
-                console.log(err); 
-            }
-        }
-        fetchData();
-    }, []);
+    // useEffect( () => {
+    //     async function fetchData() {
+    //         try {
+    //             const batchTrack = document.getElementById("batchSelect");
+    //             const fetchResponse = await fetch(`https://${backendURL}/users/list`);
+    //             const users = await fetchResponse.json();
+    //             // const users = Object.keys(data['Data'])
+    //             setUsers(users); 
+
+    //             for (let i = 0; i < users.length; i++) {
+    //                 const newOption = document.createElement("option");
+    //                 newOption.value = users[i];
+    //                 newOption.text = users[i];
+    //                 batchTrack.appendChild(newOption);
+    //             }
+
+    //         } catch (err) {
+    //             console.log(err); 
+    //         }
+    //     }
+    //     fetchData();
+    // }, []);
+
+    // call the users add endpoint using fetch
+	const login = async (e) => {
+		e.preventDefault();
+		try {
+			// TODO: make text into field variables
+			let res = await fetch(`https://${backendURL}/users/login`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				// body of the POST request
+				body: JSON.stringify({
+					username: input.username,
+					password: input.password
+				})
+			});
+			// not sure of purpose of this
+			let resJson = await res.json();
+            console.log(input);
+            console.log(resJson);
+			// reset states if ok
+			if (res.status === 200) {
+				input.username = "";
+				input.password = "";
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
 
     return (
@@ -55,17 +98,21 @@ const SignIn = () => {
                 }}
             >
                 <h1>Returning User: Sign In</h1>
-                <div className="username">
+                {/* <div className="username">
                     <label>Username     </label>
                     <select id="batchSelect" style={{height:"3.5vh", width:"20vh"}}> {users} </select>
-                </div>
+                </div> */}
                 <form>
+                    <div className="username" style={{paddingTop:"0.5rem", paddingBottom:"0.5rem"}}>
+                        <label>Username     </label>
+                        <input placeholder='Username' type="username" name="username" style={{height:"2.5vh", width:"19.3vh"}} value={input.username} onChange={onInputChange} required />
+                    </div>
                     <div className="password" style={{paddingTop:"0.5rem", paddingBottom:"0.5rem"}}>
                         <label>Password     </label>
-                        <input placeholder='Password' type="password" name="pass" style={{height:"2.5vh", width:"20vh"}} required />
+                        <input placeholder='Password' type="password" name="password" style={{height:"2.5vh", width:"20vh"}} value={input.password} onChange={onInputChange} required />
                     </div>
                     <div className="button">
-                        <input type="submit" style={{height:"3.5vh"}}/>
+                        <input type="submit" style={{height:"3.5vh"}} onClick={login}/>
                     </div>
                 </form>
                 <a href="./signup" style={{textDecoration:"none", paddingTop:"1rem"}}>
