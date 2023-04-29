ffc2f4481931aaeef81f4b79e50f830fbfeb7f7e
Yeseon02 <yk2384@nyu.edu>
2023-04-29 17:04:08 -0400

    groc type dropdown

--- src/pages/addGroc.js
+++ src/pages/addGroc.js
@@ -77,7 +77,7 @@ const AddGroc = () => {
 			backgroundAttachment: 'fixed'
 		}}
 		>
-            <button style={{fontSize:"1.5vw", paddingLeft: '15rem', paddingTop: "1rem", height:"5vh", width:"5vw", backgroundColor: "#fffccf", borderRadius: "10px", borderStyle:"None"}} Click={handleClick} onClick={()=> navigate('/groc')}>{Sign}Back</button>
+            <button style={{fontSize:"2vw", paddingLeft: '15rem', paddingTop: "1rem", height:"5vh", width:"5vw", backgroundColor: "#fffccf", borderRadius: "10px", borderStyle:"None"}} Click={handleClick} onClick={()=> navigate('/groc')}>{Sign}Back</button>
 			<div
 			style={{
 				display: 'flex',
@@ -94,14 +94,27 @@ const AddGroc = () => {
                     <div style={{paddingBottom: "0.5rem"}}>
                         <input type="text" placeholder="Item Name" name="item" value={input.item} onChange={onInputChange}></input><br></br>
                     </div>
-                    <div style={{paddingBottom: "0.5rem"}}>
+                    {/* <div style={{paddingBottom: "0.5rem"}}>
                         <input type="text" placeholder="Type" name="grocery_type" value={input.grocery_type} onChange={onInputChange}></input><br></br>
+                    </div> */}
+                    <div style={{paddingBottom: "0.5rem"}}>
+                        <select id="types" name="grocery_type" value={input.grocery_type} onChange={onInputChange}>
+                            <option value="Baked Goods">Baked Goods</option>
+                            <option value="Carbs">Carbs</option>
+                            <option value="Fruit">Fruit</option>
+                            <option value="Dairy">Dairy</option>
+                            <option value="Vegetables">Vegetables</option>
+                            <option value="Snacks">Snacks</option>
+                            <option value="Drinks">Drinks</option>
+                            <option value="Meat">Meat</option>
+                            <option value="Misc">Misc</option>
+                        </select><br></br>
                     </div>
                     <div style={{paddingBottom: "0.5rem"}}>
                         <input type="text" placeholder="Quantity" name="quantity" value={input.quantity} onChange={onInputChange}></input><br></br>
                     </div>
                     <div style={{paddingBottom: "0.5rem"}}>
-                        <input type="text" placeholder="Expiration Date" name="expiration_date" value={input.expiration_date} onChange={onInputChange}></input><br></br>
+                        <input type="date" placeholder="Expiration Date" name="expiration_date" value={input.expiration_date} onChange={onInputChange}></input><br></br>
                     </div>
                     <div onClick={addGroc}>
                         <button type = 'submit' style={{height:"2.5vh"}} onClick={()=> navigate('/groc')}>Submit</button>
